b8d7de2983eecab9a614dc4bc0426530158dd174
TusharKarsan <TusharKarsan@DESKTOP-6FVTGBN>
2016-09-01 11:29:02 +0100

    This is my other new change.....

---
+++ src/Gari/Controllers/NewJobController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Gari.Controllers
+{
+    public class NewJobController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            try
+            {
+                throw new Exception("Testing the waters");
+            }
+            catch(Exception err)
+            {
+                // Need to log this.
+            }
+            return View();
+        }
+    }
+}
