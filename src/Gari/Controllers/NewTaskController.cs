31b049789e79b5341e4cf5062fb9fb5259ae103b
TusharKarsan <TusharKarsan@DESKTOP-6FVTGBN>
2016-09-01 12:10:59 +0100

    Another change

--- src/Gari/Controllers/NewTaskController.cs
+++ src/Gari/Controllers/NewTaskController.cs
@@ -13,28 +13,8 @@ namespace Gari.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            // This is Tushar.
-            return View();
-        }
-    }
-}
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace Gari.Controllers
-{
-    public class NewTaskController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
+            // this is both changes.
             var today = DateTime.Now;
-
             return View();
         }
     }
